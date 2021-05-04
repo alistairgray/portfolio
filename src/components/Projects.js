84c43be06e98eb6a11d542098b5f14ce244881a0
alistairgray <alistair.g.gray@gmail.com>
2021-05-04 11:51:02 +1000

    added hyperlinks to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,24 +7,44 @@ function Projects() {
                 
                 <div className="project-item-row">  
                     <div className="project-item">
-                        <h2>Convo</h2>
-                        <img src="/images/convo.png" alt=""/>
+                        <a href="https://alistairgray.github.io/convo-react-frontend/"><h2>Convo</h2>
+                        <a href="https://alistairgray.github.io/convo-react-frontend/"></a><img src="/images/convo.png" alt=""/></a>
                         <p>This is an AI powered chatbot that simulates a networking conversation. Its aim is to help people who are new to networking and give them advice and encouragement for the real thing.</p>
                         <p>Tech Stack: React, Node, Express, Axios, IBM Watson (API) and Bootstrap</p>
+                        <ul>
+                            <li><a href="https://github.com/alistairgray/convo-react-frontend">Github</a></li>
+                            <li><a href="https://alistairgray.github.io/convo-react-frontend/">Live Site</a></li>                        
+                        </ul>
                     </div>
 
                     <div className="project-item">           
-                        <h2>Burning Airlines</h2>
-                        <img src="/images/burning.png"/>
+                        <a href="https://github.com/alistairgray/burning-airlines-front-end"><h2>Burning Airlines</h2></a>
+                        <a href="https://github.com/alistairgray/burning-airlines-front-end"><img src="/images/burning.png"/></a>
                         <p>An airline booking and registration app. Built during a 48hr period. This was a group project testing our resolve.</p>
                         <p>Tech Stack: React, Ruby-on-Rails and Axios</p>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/alistairgray/burning-airlines-front-end">Github Front End</a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/alistairgray/rails-burning-airlines">Github Back End</a>
+                            </li>
+                        </ul>
                     </div> 
 
                     <div className="project-item">
-                        <h2>Krave</h2>
-                        <img src="/images/krave_orig.png"/>
+                        <a href="https://github.com/alistairgray/ka-react-frontend"><h2>Krave</h2></a>
+                        <a href="https://github.com/alistairgray/ka-react-frontend"><img src="/images/krave_orig.png"/></a>
                         <p>A restaurant menu app that sought to help people search for dietary requirements.</p>
                         <p>Tech Stack: React, Ruby-on-Rails, Google Maps (API), Bootstrap and Axios</p>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/alistairgray/ka-react-frontend">Github Front End</a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/alistairgray/krave-rails-backend">Github Back End</a>
+                            </li>
+                        </ul>
                     </div>
 
                 </div> 
