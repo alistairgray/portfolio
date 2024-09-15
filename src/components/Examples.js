a49212b9a68c6cd55e3f7da46e78049e7e75d509
alistairgray <73462489+alistairgray@users.noreply.github.com>
2024-09-15 16:02:21 +0200

    Added link to blog

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -8,7 +8,7 @@ function Examples() {
                     <h2>I'm a Technical Writer</h2>
                     <h3>The majority of my client work is hidden behind NDAs, however I have included some samples of my writing below.</h3>
                     <div className='examples-item-row'>
-                        <h3> Easily handle contingencies with Command Sequences</h3>
+                        <h3><a href="https://leanspace.io/blog/easily-handle-contingencies-with-command-sequences/">Easily handle contingencies with Command Sequences</a></h3>
                         <p> This article is one of the many that I have written for Leanspace. The articles are aimed at a developers and space engineers, which means that I have to balance the level of content to reach both audiences. 
                         </p>
                         <p> The API documentation is hidden behind NDAs, however the elements used in these articles are all currently being used to educate and inform our developer customer base</p>
